45bae263969cf47da307683b053189a42d8efefe
haseebt456 <m.haseebtufail@gmail.com>
2026-08-01 04:35:35 +0500

    add handling for UDP packets

---
+++ build/CMakeFiles/dpi_engine_mt.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dpi_engine_mt.
+# This may be replaced when dependencies are built.
---
+++ build/windivert_agent/CMakeFiles/dpi_agent.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/fastech laptop/Desktop/haseeb/New folder/Deep Packet Inspector/network analyzer/network_analyzer/windivert_agent/main_windivert.cpp" "windivert_agent/CMakeFiles/dpi_agent.dir/main_windivert.cpp.obj" "gcc" "windivert_agent/CMakeFiles/dpi_agent.dir/main_windivert.cpp.obj.d"
+  "C:/Users/fastech laptop/Desktop/haseeb/New folder/Deep Packet Inspector/network analyzer/network_analyzer/windivert_agent/sni_extractor.cpp" "windivert_agent/CMakeFiles/dpi_agent.dir/sni_extractor.cpp.obj" "gcc" "windivert_agent/CMakeFiles/dpi_agent.dir/sni_extractor.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
